36646d836f196b9e1b36039b8571fb57e4af5506
Wambatcode <feyfey927@gmail.com>
2024-02-29 04:58:17 +0900

    Feat: Comment 생성 시 유저 정보 주입

--- src/main/java/com/example/firstproject/comment/controller/CommentController.java
+++ src/main/java/com/example/firstproject/comment/controller/CommentController.java
@@ -4,6 +4,7 @@ import com.example.firstproject.comment.entity.Comment;
 import com.example.firstproject.comment.entity.CommentDTO;
 import com.example.firstproject.comment.entity.CommentMapper;
 import com.example.firstproject.comment.service.CommentService;
+import com.example.firstproject.user.service.UserService;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,27 +16,32 @@ public class CommentController {
 
     private final CommentService commentService;
     private final CommentMapper commentMapper;
+    private final UserService userService;
 
-    public CommentController(CommentService commentService, CommentMapper commentMapper){
+    public CommentController(CommentService commentService, CommentMapper commentMapper, UserService userService){
         this.commentService = commentService;
         this.commentMapper = commentMapper;
+        this.userService = userService;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public String saveComment(@ModelAttribute CommentDTO commentDTO, @RequestParam Long postId, RedirectAttributes redirectAttributes){
+    @PostMapping
+    public String saveComment(@ModelAttribute CommentDTO commentDTO, @RequestParam Long postId,
+                              String userId, RedirectAttributes redirectAttributes){
         Comment comment = commentMapper.commentDTOToComment(commentDTO);
-        commentService.saveComment(postId, comment);
+        if(userService.validation(userId)){
+            commentService.saveComment(postId, comment, userId);
+        }
         redirectAttributes.addAttribute("postId", postId);
         return "redirect:/posts/{postId}";
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public String saveComment_1(@RequestBody CommentDTO commentDTO, @RequestParam Long postId, RedirectAttributes redirectAttributes){
         Comment comment = commentMapper.commentDTOToComment(commentDTO);
         commentService.saveComment(postId, comment);
         redirectAttributes.addAttribute("postId", postId);
         return "redirect:/posts/{postId}";
-    }
+    }**/
 
     @PostMapping(value = "/{commentId}/edit", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String updateComment(@PathVariable Long commentId, @ModelAttribute CommentDTO commentDTO,
@@ -45,13 +51,13 @@ public class CommentController {
         return "redirect:/posts/{postId}";
     }
 
-    @PostMapping(value = "/{commentId}/edit", consumes = MediaType.APPLICATION_JSON_VALUE)
+    /**@PostMapping(value = "/{commentId}/edit", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String updateComment_1(@PathVariable Long commentId, @RequestBody CommentDTO commentDTO,
                                 RedirectAttributes redirectAttributes){
         Comment comment = commentMapper.commentDTOToComment(commentDTO);
         redirectAttributes.addAttribute("postId", commentService.updateComment(commentId, comment).getPost().getId());
         return "redirect:/posts/{postId}";
-    }
+    }**/
 
     @DeleteMapping("/{commentId}")
     public String deleteComment(@PathVariable Long commentId){
--- src/main/java/com/example/firstproject/comment/service/CommentService.java
+++ src/main/java/com/example/firstproject/comment/service/CommentService.java
@@ -6,6 +6,8 @@ import com.example.firstproject.comment.entity.Comment;
 import com.example.firstproject.comment.repository.CommentRepository;
 import com.example.firstproject.post.entity.Post;
 import com.example.firstproject.post.repository.PostRepository;
+import com.example.firstproject.user.entity.User;
+import com.example.firstproject.user.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +19,20 @@ import java.util.Optional;
 public class CommentService {
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
+    private final UserService userService;
 
-    public CommentService(CommentRepository commentRepository, PostRepository postRepository){
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, UserService userService){
         this.commentRepository = commentRepository;
         this.postRepository = postRepository;
+        this.userService = userService;
     }
 
-    public void saveComment(Long postId, Comment comment){
+    public void saveComment(Long postId, Comment comment, String userId){
+        User user = userService.findUserByUserId(userId);
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new Exception(ExceptionEnum.POST_NOT_FOUND));
         comment.setPost(post);
+        comment.setUser(user);
         commentRepository.save(comment);
     }
 
@@ -34,11 +40,13 @@ public class CommentService {
         return commentRepository.findCommentsByPostId(postId);
     }
 
-    public Comment updateComment(Long commentId, Comment comment){
+    public Comment updateComment(Long commentId, Comment comment, String userId){
+        User user = userService.findUserByUserId(userId);
         Comment lastComment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new Exception(ExceptionEnum.COMMENT_NOT_FOUND));
         Optional.ofNullable(comment.getContent())
                 .ifPresent(lastComment::setContent);
+        lastComment.setUser(user);
 
         return commentRepository.save(lastComment);
     }
